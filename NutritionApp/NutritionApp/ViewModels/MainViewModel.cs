ea78c3a21b191e6339c05d1ad4fc85ec0ed31d5b
Qdzia <46885229+Qdzia@users.noreply.github.com>
2020-06-10 18:12:06 +0200

    Create Project

---
+++ NutritionApp/NutritionApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using NutritionApp.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionApp.ViewModels
+{
+    class MainViewModel : BaseVM
+    {
+        private string _test = "Binding Test";
+        public string BindingTest
+        {
+            get { return _test; }
+            set { SetProperty(ref _test, value); }
+        }
+    }
+}

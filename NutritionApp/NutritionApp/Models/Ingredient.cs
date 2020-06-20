13b395fc472b408d9d490049235bed4d68f01163
Dariusz Momot <44140694+zwalone@users.noreply.github.com>
2020-06-21 00:13:14 +0200

    Create Ingredient.cs

---
+++ NutritionApp/NutritionApp/Models/Ingredient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionApp.Models
+{
+    class Ingredient
+    {
+        public int count;
+        public string name;
+        public Unit unit;
+
+        public enum Unit
+        {
+            g,
+            dag,
+            kg
+        }
+
+        public Ingredient(string name, int count, Unit unit)
+        {
+            this.count = count;
+            this.name = name;
+            this.unit = unit;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} {count} {unit.ToString()}";
+        }
+    }
+}

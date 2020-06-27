b47cbc61f7f589555c1dba11565f02be7e088f39
Qdzia <46885229+Qdzia@users.noreply.github.com>
2020-06-27 12:48:56 +0200

    Modify Statistic View

--- NutritionApp/NutritionApp/ViewModels/StatisticsViewModel.cs
+++ NutritionApp/NutritionApp/ViewModels/StatisticsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using NutritionApp.Base;
+using NutritionApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,31 @@ namespace NutritionApp.ViewModels
 {
     class StatisticsViewModel: BaseVM
     {
-        public string Test { get { return "test Binding"; } }
+        public List<string> DailyIncome { get; set; }
+
+        List<string> Days;
+        List<NutritionLabel> LabelsOfWeek;
+        public NutritionLabel TotalAmount { get; set; }
+
+        public StatisticsViewModel()
+        {
+            Days = new List<string>() { "Monday ", "Tuesday " };
+            LabelsOfWeek = new List<NutritionLabel>();
+
+            LabelsOfWeek.Add(new NutritionLabel(1,2,3,4,5,6));
+            LabelsOfWeek.Add(new NutritionLabel(6,5,4,3,2,1));
+
+            TotalAmount = new NutritionLabel(6, 5, 4, 3, 2, 1);
+            UpdateView();
+        }
+
+        void UpdateView()
+        {
+            DailyIncome = new List<string>();
+            for (int i = 0; i < Days.Count; i++)
+            {
+                DailyIncome.Add(Days[i] + LabelsOfWeek[i].ToString());
+            }
+        }
     }
 }
